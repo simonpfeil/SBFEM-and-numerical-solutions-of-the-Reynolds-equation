2ac65fb0d8237b82f09afd8b8e78cac114808e6e
simonpfeil <simon.pfeil@ovgu.de>
2025-07-06 14:26:33 +0200

    Update MODULE_JPICC.f90

--- Fortran/DEMONSTRATION_FVM_ELROD/MODULE_JPICC.f90
+++ Fortran/DEMONSTRATION_FVM_ELROD/MODULE_JPICC.f90
@@ -1,16 +1,18 @@
-! This module relies on an IC factorization available under
+! This module relies on a selection of algorithms for performing an Incomplete Cholesky
+! factorization. The files listed under "CONTAINS" correspond to particular subprograms available
+! under
 !
-!  authors: Mark T. Jones, Paul E. Plassmann
-!  title: Algorithm 740: Fortran subroutines to compute improved incomplete Cholesky factorizations
-!  published in: ACM Transactions on Mathematical Software, vol. 21, no. 1, March 1995, p. 18-19
-!  publisher: Association for Computing Machinery
-!  address: New York, NY, USA
-!  issn: 0098-3500
-!  url: https://doi.org/10.1145/200979.200986
-!  doi: 10.1145/200979.200986
+!   authors: Mark T. Jones, Paul E. Plassmann
+!   title: Algorithm 740: Fortran subroutines to compute improved incomplete Cholesky factorizations
+!   published in: ACM Transactions on Mathematical Software, vol. 21, no. 1, March 1995, p. 18-19
+!   publisher: Association for Computing Machinery
+!   address: New York, NY, USA
+!   issn: 0098-3500
+!   url: https://doi.org/10.1145/200979.200986
+!   doi: 10.1145/200979.200986
 !
-! Please download the subprograms listed under "CONTAINS" and place them in the folder
-! "./jones_plassmann_jpicc".
+! Please download the subprograms from there and place them in the folder "./jones_plassmann_jpicc"
+! using these file names.
 
 MODULE MODULE_JPICC
   
