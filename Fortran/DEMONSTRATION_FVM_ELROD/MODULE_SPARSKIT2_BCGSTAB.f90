5497a2863e6a8d10496f9832b6dce51b83b965e1
simonpfeil <simon.pfeil@ovgu.de>
2025-07-06 14:25:55 +0200

    Update MODULE_SPARSKIT2_BCGSTAB.f90

--- Fortran/DEMONSTRATION_FVM_ELROD/MODULE_SPARSKIT2_BCGSTAB.f90
+++ Fortran/DEMONSTRATION_FVM_ELROD/MODULE_SPARSKIT2_BCGSTAB.f90
@@ -2,9 +2,9 @@
 ! BCGSTAB. The files listed under "CONTAINS" correspond to particular subprograms available
 ! under
 !
-!  Yousef Saad: SPARSKIT2. University of Minnesota, Department of Computer Science and Engineering, 
-!  200 Union Street S.E., Minneapolis, MN 55455 USA, [saad -at- umn -dot- edu],
-!  https://www-users.cse.umn.edu/~saad/software/SPARSKIT/
+!   Yousef Saad: SPARSKIT2. University of Minnesota, Department of Computer Science and Engineering, 
+!   200 Union Street S.E., Minneapolis, MN 55455 USA, [saad -at- umn -dot- edu],
+!   https://www-users.cse.umn.edu/~saad/software/SPARSKIT/
 !
 ! Please download the subprograms from there and place them in the folder "./sparskit2_bcgstab" 
 ! using these file names.
