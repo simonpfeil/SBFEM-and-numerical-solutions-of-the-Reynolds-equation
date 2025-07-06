46ee8d180a991f687baf14e2cadfd98b15752261
simonpfeil <simon.pfeil@ovgu.de>
2025-07-06 13:48:37 +0200

    Delete Fortran/DEMONSTRATION_FVM_ELROD/sparskit2_bcgstab directory

--- Fortran/DEMONSTRATION_FVM_ELROD/sparskit2_bcgstab/distdot.f90
+++
@@ -1,8 +0,0 @@
-      function distdot(n,x,ix,y,iy) 
-      integer n, ix, iy 
-      !real*8 distdot, x(*), y(*), ddot 
-	  real*8 distdot, x(*), y(*)
-      !external ddot 
-      distdot = ddot(n,x,ix,y,iy) 
-      return 
-      END             function                              
