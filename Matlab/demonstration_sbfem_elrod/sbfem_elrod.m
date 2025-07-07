6112d5c91938a743896d01e3a674ac80ee54b995
simonpfeil <simon.pfeil@ovgu.de>
2025-07-07 17:27:44 +0200

    Update sbfem_elrod.m

--- Matlab/demonstration_sbfem_elrod/sbfem_elrod.m
+++ Matlab/demonstration_sbfem_elrod/sbfem_elrod.m
@@ -403,8 +403,8 @@ nodes_dof_vec = nonzeros(nodes_dof_vec);                                   % vec
 
 
 if p_os >= 0                                                               % "IF a non-negative supply pressure is prescribed, then ..."
-	Pi_os = p_os/p_ref;                                                % nondimensionalize the supply pressure and save as pressure-like function
-	g_os = 1;                                                          % set corresponding switch function to 1
+    Pi_os = p_os/p_ref;                                                    % nondimensionalize the supply pressure and save as pressure-like function
+    g_os = 1;                                                              % set corresponding switch function to 1
 else                                                                       % otherwise, the prescribed value p_os will be interpreted as a film fraction with an offset of -1, i.e., as a negative pressure-like function
     Pi_os = p_os;                                                          % save prescribed value as pressure-like function
     g_os = 0;                                                              % set corresponding switch function to 0
